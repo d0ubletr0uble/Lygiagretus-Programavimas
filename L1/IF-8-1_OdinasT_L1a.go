d18a3470c25d2aac266b7f6aef008b2c2fb64bbb
Tomas Odinas <tomas.odinas@ktu.edu>
2020-09-04 15:05:49 +0300

    L1a data read step done

--- L1/IF-8-1_OdinasT_L1a.go
+++ L1/IF-8-1_OdinasT_L1a.go
@@ -1,14 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
 
-type DataMonitor struct {
-	name string
-	age int
-	average float64
+type Car struct {
+	Make    string  `json:"make"`
+	Year    int     `json:"year"`
+	Mileage float64 `json:"mileage"`
 }
 
-
 func main() {
-	fmt.Println("hello")
+	cars := readData("IF-8-1_OdinasT_L1_dat_1.json")
+
+	fmt.Println(cars)
+}
+
+func readData(path string) []Car{
+	data, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		panic(fmt.Sprintf("error when reading file %s", path))
+	}
+
+	var cars []Car
+	err = json.Unmarshal(data, &cars)
+
+	if err != nil {
+		panic(fmt.Sprintf("error converting json in file %s", path))
+	}
+
+	return cars
 }
